d1327b83458cadaabbe007dd19c903d4ea1ddd7d
dile <duyle131103@gmail.com>
2025-08-15 22:51:10 +0700

    feat: Implement phone number sign-in and verification, integrating the API for sign-in using phone number

---
+++ app/verify-phone/page.tsx
@@ -0,0 +1,74 @@
+"use client";
+import { API_URL } from "@/constants/api";
+import { errorMessages } from "@/constants/error";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function PhoneVerify() {
+  const [phone, setPhone] = useState('');
+  const [accessCode, setAccessCode] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const router = useRouter();
+
+  useEffect(() => {
+    const storedPhone = localStorage.getItem('phone');
+    if (storedPhone) {
+      setPhone(storedPhone);
+    }
+  }, []);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const trimmedAccessCode = accessCode.replace(/\s+/g, '');
+
+    if (trimmedAccessCode.length !== 6 || !/^[0-9]+$/.test(trimmedAccessCode)) {
+      setError(errorMessages.INVALID_ACCESS_CODE);
+      return;
+    }
+    
+    try {
+      setLoading(true);
+      setError('');
+
+      await axios.post(
+        `${API_URL}/auth/validateAccessCode`,
+        { phone, accessCode: trimmedAccessCode }
+      );
+      router.push('/dashboard');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className="auth-container">
+      <div className="auth-wrapper">
+        <h2 className="section-heading">Phone Verification</h2>
+        <p className="mb-6 text-center text-[#a0a0a0]">
+          Please enter your code that send to your phone
+        </p>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter Your Code"
+            className="input"
+            onChange={(e) => setAccessCode(e.target.value)}
+            disabled={loading}
+          />
+          {error && <span className="error">{error}</span>}
+          <button 
+            type="submit" 
+            className="btn btn-primary" 
+            disabled={loading}>
+            {loading ? 'Verifying...' : 'Submit'}
+          </button>
+        </form>
+        <p className="p-sm mt-4">
+          Code not received? <a className="text-blue-600 cursor-pointer">Send again</a>
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ constants/error.js
@@ -0,0 +1,4 @@
+export const errorMessages = {
+  INVALID_PHONE_NUMBER: 'Only numbers are allowed and must be exactly 11 digits!',
+  INVALID_ACCESS_CODE: 'Invalid access code! Please enter a valid 6-digit code.',
+};
